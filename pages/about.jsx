bf4205f241af547076b3cfcfd4b2aa5440de1212
Jean-OIKONOMOU <jean.oikonomou@gmail.com>
2020-02-20 22:17:34 +0100

    added new page

---
+++ pages/about.jsx
@@ -0,0 +1,17 @@
+const About = () => (
+
+    <div>
+        <h1>
+            what is this ABOUT ?
+        </h1>
+    </div>
+)
+
+function something() {
+    const name = "Dave"
+    return (
+        <h2>I'm another ABOUT THING! - {name}</h2>
+    )
+}
+
+export default something
